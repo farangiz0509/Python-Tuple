7311b042769cd1f47ba97dec76087235897e07e2
farangiz0509 <farangiztolibova75@gmail.com>
2025-07-04 14:41:18 +0545

    done

---
+++ 02.py
@@ -0,0 +1,14 @@
+students =  [
+    ("Ali", ["Fizika", "Matematika"]),
+     ("Laylo", ["Ingliz tili"]), 
+     ("Jasur", ["Matematika", "Informatika"])
+]
+
+subject = input()
+
+count = 0
+for student in students:
+    if subject in student[1]:
+        count += 1
+
+print(count)
